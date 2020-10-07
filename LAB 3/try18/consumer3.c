fba181f14bad0b0d8e54dd4ef0f39b7e56b5d5ad
Aditya-2001 <iit2019210@iiita.ac.in>
2020-10-08 00:06:36 +0530

    bug fixed, mutex lock is creating some problem in the semaphore and signal is not reaching properly

---
+++ LAB 3/try18/consumer3.c
@@ -0,0 +1,54 @@
+/*THIS IS THE CONSUMER PROCESS*/
+/*IN THIS EXAMPLE START THE CONSUMER PROCESS FIRST*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<signal.h>
+#include<sys/shm.h>
+#include<sys/ipc.h>
+#include<unistd.h>
+
+typedef struct {
+  int x;
+  int f;
+  int prid;
+  int cnid;
+  int flag;
+}mydata;
+
+mydata * data;
+
+void my_handler(){}
+
+int main(){
+  printf("Consumer Program Started...\n");
+  signal(SIGUSR1,my_handler);
+  int status, i, n;
+  int shmid;
+	key_t key;
+	key = ftok("buffer.txt", 12);
+	if(key<0){
+		perror("error0:");
+		exit(1);
+	}
+	shmid = shmget(key, sizeof(mydata), IPC_CREAT | 0666);
+	if(shmid<0){
+		perror("error1:");
+		exit(1);
+	}
+	data = (mydata *) shmat(shmid, NULL, 0);
+	if(data == (void *) -1){
+		perror("error2:");
+		exit(1);
+	}
+
+  data->cnid = getpid();
+  n = 10000;
+  pause();
+
+  for(i = 1; i<=n; i++){
+    if(data->flag==1) pause();
+		(data->x)--;
+		data->flag=1;
+		kill(data->prid, SIGUSR1);
+  }
+}
